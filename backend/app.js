88ea26ef99afbc74059ba091b51f9ce0d7c9d586
sanjula97 <sanjulakarunarathna456@gmail.com>
2020-01-11 11:17:29 +0530

    second commit, added authentication and storage functionality

--- backend/app.js
+++ backend/app.js
@@ -21,3 +21,46 @@ db.once('open', function() {
   console.log('Connected dude!');
 });
 
+const checkAuth = require('../backend/middleware/user-auth');
+
+app.use(bodyParser.json());
+app.use(bodyParser.urlencoded({ extended: false }));
+app.use(cors());
+
+app.use('/images', express.static(path.join('backend/images')));
+
+// app.use((req, res, next) => {
+//     res.header('Access-Control-Allow-Origin', "*");
+//     res.header('Access-Control-Allow-Headers', true);
+//     res.header('Access-Control-Allow-Credentials', 'Content-Type');
+//     res.header('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE');
+//     next();
+// });
+
+// Multer config
+const MIME_TYPE_MAP = {
+    'image/png': 'png',
+    'image/jpeg': 'jpg',
+    'image/jpg': 'jpg'
+};
+
+const storage = multer.diskStorage({
+    destination: (req, file, cb) => {
+        const isValid = MIME_TYPE_MAP[file.mimetype];
+        let error = null;
+        if (!isValid) {
+            error = 'INVALID FILE TYPE!';
+        }
+
+        cb(error, 'backend/images');
+    },
+    filename: (req, file, cb) => {
+        let name = file.originalname.toLowerCase().split(' ').join('-');
+        name = name.substring(0, name.indexOf('.'));
+        const ext = MIME_TYPE_MAP[file.mimetype];
+        cb(null, name + '-' + Date.now() + '.' + ext);
+    }
+});
+
+
+
